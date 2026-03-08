85c24da56d2e38d2251e1736d43663224c9f3488
Dheh3 <dhehe361@gmail.com>
2026-03-08 17:29:42 -0300

    first fast api def

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from dotenv import load_dotenv
 import os
+from fastapi import FastAPI
 
 load_dotenv()
+app = FastAPI()
 
 print(os.getenv("USER_TEST"))
+print("="*30)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id:int, q:str | None = None):
+    return {"item_id": item_id, "q": q}
